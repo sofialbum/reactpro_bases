bf5abb761c90a41b19953c9678472f92b3ff6e1a
Sofía González <sofia_1.6@hotmail.com>
2024-06-04 13:05:40 -0300

    objectState

---
+++ src/bases/Counter.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+interface Props {
+  initialValue: number
+}
+
+const Counter = ({ initialValue = 0 }: Props) => {
+
+  const [counter, setCounter] = useState(initialValue);
+
+  const handleClick = () => {
+    setCounter(prev => prev + 1)
+  }
+  return (
+    <>
+      <h1>Counter: {counter}</h1>
+      <button onClick={handleClick}>+1</button>
+    </>
+  )
+}
+
+export default Counter
---
+++ src/bases/CounterBy.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+interface Props {
+  initialValue?: number
+}
+
+const CounterBy = ({ initialValue =  5 }: Props) => {
+
+  const [counterState, setCounterState] = useState({
+    counter: initialValue,
+    clicks: 0,
+  });
+
+  const { counter, clicks } = counterState;
+
+  const handleClick = (value: number) => {
+    setCounterState( prev => ({
+      counter: prev.counter + value,
+      clicks: prev.clicks + 1
+    }))
+  }
+
+  return (
+    <>
+      <h1>CounterBy: { counter }</h1>
+      <h1>Clicks: { clicks }</h1>
+
+      <button onClick={ () => handleClick(1) }>+1</button>
+      <button onClick={ () => handleClick(5) }>+5</button>
+    </>
+  )
+}
+
+export default CounterBy;
